ebe1387f02ba19a096be463e140035c9d6abdead
Ian Thompson <thepi200@gmail.com>
2011-12-04 16:09:57 -0500

    Updated drawables, layout, and colors to take advantage of TeamView

--- gen/org/team639/scouting/R.java
+++ gen/org/team639/scouting/R.java
@@ -29,13 +29,15 @@ containing a value of this type.
     public static final class color {
         public static final int blueAlliance=0x7f040001;
         public static final int blueAllianceDim=0x7f040008;
-        public static final int blueAllianceText=0x7f040003;
         public static final int blueAllianceTextDim=0x7f040005;
+        public static final int blueAllianceTextNorm=0x7f040003;
+        public static final int blue_alliance_text=0x7f04000b;
         public static final int redAlliance=0x7f040000;
         public static final int redAllianceDim=0x7f040007;
-        public static final int redAllianceText=0x7f040002;
         public static final int redAllianceTextDim=0x7f040004;
+        public static final int redAllianceTextNorm=0x7f040002;
         public static final int redCard=0x7f040009;
+        public static final int red_alliance_text=0x7f04000c;
         public static final int winAlliance=0x7f040006;
         public static final int yellowCard=0x7f04000a;
     }
@@ -44,29 +46,35 @@ containing a value of this type.
     }
     public static final class drawable {
         public static final int blue_left=0x7f020000;
-        public static final int blue_left_red=0x7f020001;
-        public static final int blue_left_yellow=0x7f020002;
-        public static final int blue_lose=0x7f020003;
-        public static final int blue_mid=0x7f020004;
-        public static final int blue_mid_red=0x7f020005;
-        public static final int blue_mid_yellow=0x7f020006;
-        public static final int blue_right=0x7f020007;
-        public static final int blue_right_red=0x7f020008;
-        public static final int blue_right_yellow=0x7f020009;
-        public static final int blue_win=0x7f02000a;
-        public static final int ic_launcher=0x7f02000b;
-        public static final int match_time=0x7f02000c;
-        public static final int red_left=0x7f02000d;
-        public static final int red_left_red=0x7f02000e;
-        public static final int red_left_yellow=0x7f02000f;
-        public static final int red_lose=0x7f020010;
-        public static final int red_mid=0x7f020011;
-        public static final int red_mid_red=0x7f020012;
-        public static final int red_mid_yellow=0x7f020013;
-        public static final int red_right=0x7f020014;
-        public static final int red_right_red=0x7f020015;
-        public static final int red_right_yellow=0x7f020016;
-        public static final int red_win=0x7f020017;
+        public static final int blue_left_none=0x7f020001;
+        public static final int blue_left_red=0x7f020002;
+        public static final int blue_left_yellow=0x7f020003;
+        public static final int blue_lose=0x7f020004;
+        public static final int blue_mid=0x7f020005;
+        public static final int blue_mid_none=0x7f020006;
+        public static final int blue_mid_red=0x7f020007;
+        public static final int blue_mid_yellow=0x7f020008;
+        public static final int blue_right=0x7f020009;
+        public static final int blue_right_none=0x7f02000a;
+        public static final int blue_right_red=0x7f02000b;
+        public static final int blue_right_yellow=0x7f02000c;
+        public static final int blue_win=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int match_time=0x7f02000f;
+        public static final int red_left=0x7f020010;
+        public static final int red_left_none=0x7f020011;
+        public static final int red_left_red=0x7f020012;
+        public static final int red_left_yellow=0x7f020013;
+        public static final int red_lose=0x7f020014;
+        public static final int red_mid=0x7f020015;
+        public static final int red_mid_none=0x7f020016;
+        public static final int red_mid_red=0x7f020017;
+        public static final int red_mid_yellow=0x7f020018;
+        public static final int red_right=0x7f020019;
+        public static final int red_right_none=0x7f02001a;
+        public static final int red_right_red=0x7f02001b;
+        public static final int red_right_yellow=0x7f02001c;
+        public static final int red_win=0x7f02001d;
     }
     public static final class id {
         public static final int B1=0x7f070009;
